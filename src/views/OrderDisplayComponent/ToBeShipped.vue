1243f1699491a1fac88f252f524cd19ca8cf8910
zhangzili <21302010078@m.fudan.edu.cn>
2023-05-19 23:41:39 +0800

    测试提交

---
+++ src/views/OrderDisplayComponent/ToBeShipped.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "ToBeShipped"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/OrdersDisplay.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "ordersDisplay"
+}
+</script>
+
+<style scoped>
+
+</style>
