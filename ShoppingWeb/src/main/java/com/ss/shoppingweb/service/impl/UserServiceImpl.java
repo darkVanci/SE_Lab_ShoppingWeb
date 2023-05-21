0f331a170a8a171f7d3e57c631380f211785d2ba
Wang Zheng <1585781755@qq.com>
2023-05-21 22:36:38 +0800

    滕储华的修改

--- ShoppingWeb/src/main/java/com/ss/shoppingweb/service/impl/UserServiceImpl.java
+++ ShoppingWeb/src/main/java/com/ss/shoppingweb/service/impl/UserServiceImpl.java
@@ -649,10 +649,7 @@ public class UserServiceImpl implements UserService {
     /**删除购物车*/
     @Override
     public void deleteShoppingcartByUserIdAndCommodityId(Integer userId, Integer commodityId) {
-        Integer rows=userMapper.deleteShoppingcartByUserIdAndCommodityId(userId,commodityId);
-        if(rows!=1){
-            throw new UpdateException("删除购物车失败，请联系系统管理员！");
-        }
+        userMapper.deleteShoppingcartByUserIdAndCommodityId(userId,commodityId);
     }
 
 
